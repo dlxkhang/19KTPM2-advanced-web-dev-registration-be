8500ed3ee38c718e2679755af14f3bfa23f21fb7
dlxkhang <khangduong0902@gmail.com>
2022-10-29 11:43:15 +0700

    Enable cors

--- app.js
+++ app.js
@@ -9,12 +9,23 @@ const route = require("./routes");
 
 const app = express();
 
-app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// Cors option
+const options = {
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+  // allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept",
+};
+
+app.use(cors(options));
+// enabling pre-flight
+// app.options("http://localhost:3000", cors(options));
 
 //routes init
 route(app);
